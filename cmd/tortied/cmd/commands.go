d1cded815e7cdf94a3a50940098c13c25d2dee01
tarunharit <pjrkjbftg6@privaterelay.appleid.com>
2025-12-04 18:12:18 -0500

    Update commands.go

--- cmd/tortied/cmd/commands.go
+++ cmd/tortied/cmd/commands.go
@@ -24,7 +24,7 @@ import (
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 	genutilcli "github.com/cosmos/cosmos-sdk/x/genutil/client/cli"
 
-	"tortie/app"
+	"github.com/tarunharit/tortie-chain/app"
 )
 
 func initRootCmd(
